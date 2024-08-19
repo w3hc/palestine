d5bd75b5724eab33574a4929994fa3b57507c7b2
julienbrg <julien@strat.cc>
2024-08-19 19:20:06 +0200

    add qr code

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import { Text, VStack } from '@chakra-ui/react'
+import { Text, VStack, Box, Center } from '@chakra-ui/react'
 import { LinkComponent } from '../../components/layout/LinkComponent'
-import Head from 'next/head'
+import Image from 'next/image'
 
 export default function About() {
   return (
-    <>
-      <Head>
-        <title>About - Palestine</title>
-      </Head>
-      <main>
-        <VStack spacing={6} align="stretch">
-          <Text fontSize="xl">The home page of this website displays the name of the persons killed in Gaza since October 7, 2023. </Text>
-          <Text fontSize="xl">
-            I want to thank all contributors involved in building and maintaining this dataset. You can learn more about it by checking Tech for
-            Palestine <LinkComponent href="https://data.techforpalestine.org">website</LinkComponent>,{' '}
-            <LinkComponent href="https://github.com/TechForPalestine/palestine-datasets">GitHub repo</LinkComponent>, and{' '}
-            <LinkComponent href="https://data.techforpalestine.org/docs/killed-in-gaza">docs</LinkComponent>.
-          </Text>
-          <Text fontSize="xl">
-            Tech for Palestine updates the list daily as new reports are released. The main sources are Gaza&apos;s Ministry of Health and Gaza&apos;s
-            Government Media Office.
-          </Text>
-          <Text fontSize="xl">Feel free to contact me if you want to add an event in the timeline, report a bug, or suggest an improvement.</Text>
-        </VStack>
-      </main>
-    </>
+    <main>
+      <VStack spacing={6} align="stretch">
+        <Text fontSize="xl">The home page of this website displays the names of people killed in Gaza since October 7, 2023.</Text>
+        <Text fontSize="xl">
+          I want to thank all contributors involved in building and maintaining this dataset. You can learn more about it by checking the Tech for
+          Palestine <LinkComponent href="https://data.techforpalestine.org">website</LinkComponent>,{' '}
+          <LinkComponent href="https://github.com/TechForPalestine/palestine-datasets">GitHub repo</LinkComponent>, and{' '}
+          <LinkComponent href="https://data.techforpalestine.org/docs/killed-in-gaza">docs</LinkComponent>.
+        </Text>
+        <Text fontSize="xl">
+          Tech for Palestine updates the list daily as new reports are released. The main sources are the Gaza Ministry of Health and the Government
+          Media Office.
+        </Text>
+        <Text fontSize="xl">Feel free to contact me if you want to add an event to the timeline, report a bug, or suggest an improvement.</Text>
+        <br />
+        <Center>
+          <VStack>
+            <Box position="relative" width="200px" height="200px" overflow="hidden" borderRadius="lg">
+              <Image priority layout="fill" objectFit="cover" alt="qr-code" src="/palestine-li.png" />
+            </Box>
+            <Text>http://palestine.li</Text>
+          </VStack>
+        </Center>
+        <br />
+        <br />
+      </VStack>
+    </main>
   )
 }
