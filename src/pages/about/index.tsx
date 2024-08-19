1cfed09a4c11b0c8481a7c05caa334b475ae4d5b
Julien Béranger <julien@strat.cc>
2024-08-19 11:58:59 +0200

    Add timeline (#8)
    * add timeline
    
    * fix source
    
    * fix theme icon
    
    * add form

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,25 +1,69 @@
-import { Text } from '@chakra-ui/react'
+import React from 'react'
+import { Text, VStack, Input, Textarea, Button, FormControl, FormLabel, useColorModeValue, useToast } from '@chakra-ui/react'
 import { LinkComponent } from '../../components/layout/LinkComponent'
 
 export default function About() {
+  const inputBg = useColorModeValue('white', 'gray.700')
+  const inputColor = useColorModeValue('gray.800', 'white')
+  const toast = useToast()
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    toast({
+      title: 'Message sent',
+      description: 'Your message was properly sent. Thank you for your input!',
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    })
+  }
+
   return (
     <>
       <main>
-        <Text>
-          The home page of this website displays the name of <strong>28,185 people killed in Gaza</strong> since October 7, 2023.{' '}
-        </Text>
-        <br />
-        <Text>
-          I want to thank all contributors involved in building this dataset. You can learn more about it by checking Tech for Palestine{' '}
-          <LinkComponent href="https://data.techforpalestine.org">website</LinkComponent>,{' '}
-          <LinkComponent href="https://github.com/TechForPalestine/palestine-datasets">GitHub repo</LinkComponent>, and{' '}
-          <LinkComponent href="https://data.techforpalestine.org/docs/killed-in-gaza">docs</LinkComponent>.
-        </Text>
-        <br />
-        <Text>
-          Tech for Palestine updates the list daily as new reports are released. On this website, updates are made on a weekly basis. The main sources
-          are Gaza&apos;s Ministry of Health and Gaza&apos;s Government Media Office.
-        </Text>
+        <VStack spacing={6} align="stretch">
+          <Text fontSize="xl">The home page of this website displays the name of the persons killed in Gaza since October 7, 2023. </Text>
+          <Text fontSize="xl">
+            I want to thank all contributors involved in building and maintaining this dataset. You can learn more about it by checking Tech for
+            Palestine <LinkComponent href="https://data.techforpalestine.org">website</LinkComponent>,{' '}
+            <LinkComponent href="https://github.com/TechForPalestine/palestine-datasets">GitHub repo</LinkComponent>, and{' '}
+            <LinkComponent href="https://data.techforpalestine.org/docs/killed-in-gaza">docs</LinkComponent>.
+          </Text>
+          <Text fontSize="xl">
+            Tech for Palestine updates the list daily as new reports are released. The main sources are Gaza&apos;s Ministry of Health and Gaza&apos;s
+            Government Media Office.
+          </Text>
+          <Text fontSize="xl">Feel free to contact me if you want to add an event in the timeline, report a bug, or suggest an improvement:</Text>
+          <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+            <input type="hidden" name="form-name" value="contact" />
+            <VStack spacing={4} align="stretch">
+              <FormControl isRequired>
+                <FormLabel htmlFor="name" fontSize="md">
+                  Name
+                </FormLabel>
+                <Input type="text" name="name" id="name" bg={inputBg} color={inputColor} fontSize="md" />
+              </FormControl>
+
+              <FormControl isRequired>
+                <FormLabel htmlFor="email" fontSize="lg">
+                  Email
+                </FormLabel>
+                <Input type="email" name="email" id="email" bg={inputBg} color={inputColor} fontSize="md" />
+              </FormControl>
+
+              <FormControl isRequired>
+                <FormLabel htmlFor="message" fontSize="md">
+                  Message
+                </FormLabel>
+                <Textarea name="message" id="message" bg={inputBg} color={inputColor} fontSize="md" />
+              </FormControl>
+
+              <Button type="submit" colorScheme="blue" fontSize="md">
+                Send
+              </Button>
+            </VStack>
+          </form>
+        </VStack>
       </main>
     </>
   )
--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -1,11 +1,93 @@
-import { Text, Button, useToast } from '@chakra-ui/react'
+import React from 'react'
+import { Box, VStack, Text, Flex, useColorModeValue, Link, Heading } from '@chakra-ui/react'
 
-export default function Timeline() {
+// TODO: Add more events (select attacks, UN resolutions, etc.)
+const timelineEvents = [
+  {
+    date: 'July 24, 2024',
+    event: 'Netanyahu is invited to speak to the US Congress.',
+    link: 'https://www.youtube.com/watch?v=rKkcstwPd9k',
+  },
+  {
+    date: 'July 9, 2024',
+    event: 'ICJ issues advisory opinion confirming West Bank and East Jerusalem as occupied territories.',
+    link: 'https://www.icj-cij.org/case/131',
+  },
+  {
+    date: 'April 24, 2004',
+    event: "Bidens signs a $26 billion USD 'aid' for Israel.",
+    link: 'https://www.washingtonpost.com/world/2024/04/24/israel-hamas-war-news-gaza-palestine/',
+  },
+  {
+    date: 'March 1, 2024',
+    event: 'ICC Prosecutor Karim Khan submits an application for arrest warrants against Netanyahu.',
+    link: 'https://www.youtube.com/watch?v=tdJNAgI7e0I',
+  },
+  {
+    date: 'January 26, 2024',
+    event: "ICJ orders provisional measures in response to South Africa's genocide case against Israel.",
+    link: 'https://www.icj-cij.org/case/192',
+  },
+  {
+    date: 'October 7, 2023',
+    event: 'Operation Al-Aqsa Flood results in approximately 1,200 deaths in Israel and around 240 hostages taken.',
+    link: 'https://www.youtube.com/watch?v=_0atzea-mPY',
+  },
+]
+
+const TimelineComponent = () => {
+  const bgColor = useColorModeValue('gray.100', 'gray.700')
+  const lineColor = '#149954'
+  const textColor = useColorModeValue('gray.800', 'gray.200')
+
   return (
-    <>
-      <main>
-        <Text>[ Under dev... ]</Text>
-      </main>
-    </>
+    <Box position="relative" paddingBottom="100vh">
+      <VStack spacing={0} position="relative" alignItems="center">
+        <Box position="absolute" left="50%" top="0" bottom="-1000vh" width="2px" bg={lineColor} zIndex={-1} />
+
+        {timelineEvents.map((event, index) => (
+          <Flex key={index} width="100%" justify={index % 2 === 0 ? 'flex-end' : 'flex-start'} mb={8}>
+            <Link href={event.link} isExternal width="45%" _hover={{ textDecoration: 'none' }}>
+              <Box
+                bg={bgColor}
+                p={4}
+                borderRadius="md"
+                boxShadow="md"
+                position="relative"
+                transition="all 0.2s"
+                _hover={{
+                  transform: 'scale(1.02)',
+                  boxShadow: 'lg',
+                }}>
+                <Text fontWeight="bold">{event.date}</Text>
+                <br />
+                <Text>{event.event}</Text>
+                <Box
+                  position="absolute"
+                  top="50%"
+                  left={index % 2 === 0 ? '-8px' : 'auto'}
+                  right={index % 2 === 0 ? 'auto' : '-8px'}
+                  width="16px"
+                  height="16px"
+                  borderRadius="50%"
+                  bg={lineColor}
+                  transform="translateY(-50%)"
+                />
+              </Box>
+            </Link>
+          </Flex>
+        ))}
+
+        <br />
+
+        <Box width="100%" textAlign="center" mt={16} mb={8}>
+          <Heading as="h2" size="2xl" color={textColor} fontWeight="bold">
+            History did not start on October 7th
+          </Heading>
+        </Box>
+      </VStack>
+    </Box>
   )
 }
+
+export default TimelineComponent
